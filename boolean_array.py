95debc91da576ac070ad6533e845056d020ce520
Ahadul Haque Shovo <suvo10.info@gmail.com>
2025-07-06 18:17:40 +0600

    Create boolean_array.py

---
+++ boolean_array.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data={'Name':['John','Karim','Rahim','Karime','Kutub','MOSHA'],
+      'Age':[25,30,28,33,56,55],
+      'city':['New York','Paris','London','Syedney','Tokiyo','Salanga'],
+      'Qualifications':['BSC','BBA',',MBA','BSC','SSC','HSC']
+      }
+df=pd.DataFrame(data)
+boolean_index=df.loc[df['Age']>30]
+print("Filtered DataFrame:")
+print(boolean_index)
