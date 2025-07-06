da82a5075eba873fad7a173777ae08faaee07aa7
Ahadul Haque Shovo <shovo2522@gmail.com>
2025-07-06 14:37:42 +0600

    Create array.py

---
+++ array.py
@@ -0,0 +1,16 @@
+import pandas as pd
+data=[2,4,6,8]
+array1=pd.array(data)
+print(array1)
+
+
+float_array=pd.array([1.1,2.2,3.3,4.4],dtype='float')
+print(float_array)
+print()
+
+string_array=pd.array(['apple','banana','cherry','date'],dtype='str')
+print(string_array)
+print()
+
+bool_array=pd.array(['True','False','True','False','True'],dtype='bool')
+print()
