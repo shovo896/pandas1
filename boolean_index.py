c5a2706c489bef76ce2182625730b935aef11f91
Ahadul Haque Shovo <suvo10.info@gmail.com>
2025-07-08 16:13:21 +0600

    boolean_index.py

---
+++ boolean_index.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+data={'Name':['John','Karim','Rahim','Karime','Kutub','MOSHA'],
+      'Age':[25,30,28,33,56,55],
+      'city':['New York','Paris','London','Syedney','Tokiyo','Salanga'],
+      'Qualifications':['BSC','BBA',',MBA','BSC','SSC','HSC']
+      }
+df=pd.DataFrame(data)
+
+boolean_index=df.loc[df['Age']>30]
+print("Filtered  Dataframe")
+print(boolean_index)
