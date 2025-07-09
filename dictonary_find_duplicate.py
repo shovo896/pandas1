429f0380ed4154e6b59a36dca44c6a2a844f2e12
Ahadul Haque Shovo <suvo10.info@gmail.com>
2025-07-09 08:38:40 +0600

    dictonary_find_duplicate.py

---
+++ dictonary_find_duplicate.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data={'Name':['John','Karim','Rahim','Karime','Kutub','MOSHA'],
+      'Age':[25,30,28,33,56,55],
+      'city':['New York','Paris','London','Syedney','Tokiyo','Salanga'],
+      'Qualifications':['BSC','BBA',',MBA','BSC','SSC','HSC']
+      }
+df=pd.DataFrame(data)
+
+
+#df for duplicate entries
+print(df.duplicated())
+
+#check for duplicate entries in columns and  Age
+print(df.duplicated(subset=['Name','Age']))
