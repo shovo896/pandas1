e2301f1b2d1bb87f3e4d006c2b880c19a519956e
Ahadul Haque Shovo <suvo10.info@gmail.com>
2025-07-13 11:16:33 +0600

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,29 @@
+import pandas  as pd
+data1={
+    'EmployeeID':['E001','E002','E003','E004','E005'],
+    'Name':['John Doe','John Doe','Rahat Khan','Mojila '],
+    'Dept ID':['D001','D002','D003','D004','DF005']
+    }
+employees=pd.DataFrame(data1)
+data2={
+    'Dept ID': ['D001','D002','D003'],
+    'DeptName':['sales','HR','Admin']
+
+
+
+
+}
+departments=pd.DataFrame(data2)
+merged_df=pd.merge(employees,departments)
+
+print("Employees:")
+print(employees)
+print()
+
+print("Departments:")
+print(departments)
+print()
+
+
+print("Merged DataFrame:")
+print(merged_df)
